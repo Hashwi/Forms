ea50927578cbb851eb72a352fab9127fb66f819d
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-07-21 17:53:39 +0200

    initiate form

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>
+          <h1>Hello World!</h1>
+          <form method="GET">
+            <div>
+              <input type="text" name = "firstName"/>
+            </div>
+            <div>
+              <input type="checkbox" name ='isOk'/>
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
       </header>
     </div>
   );
-
 }
-
 
 export default App;
